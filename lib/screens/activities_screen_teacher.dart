47eb158004e2a579c7444cc04bd0dbd30dafd15a
BasantaPoudel <basanta.poudel@tecnico.ulisboa.pt>
2024-04-15 20:16:31 +0300

    Refactor

--- lib/screens/activities_screen_teacher.dart
+++ lib/screens/activities_screen_teacher.dart
@@ -5,7 +5,6 @@ import 'package:feedback_flow/cubits/database_screen/database_screen_state.dart'
 import 'package:feedback_flow/models/activity.dart';
 import 'package:feedback_flow/models/rubric.dart';
 import 'package:feedback_flow/screens/rubric_screen.dart';
-import 'package:feedback_flow/screens/score_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
